f89e16401ab86ec02147d3c74f703705dee5fa8d
Sukanta Das <arnobh84@gmail.com>
2023-02-16 21:59:45 +0600

    will work tommorow

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import CreateNote from "../../Components/CreateNote/CreateNote";
+import Notes from "../../Components/Notes/Notes";
 
 const Home = () => {
   return (
     <div>
       <CreateNote />
+      <div className="w-[90%] mx-auto mt-10">
+        <Notes />
+      </div>
     </div>
   );
 };
--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import SignIn from "../Pages/Auth/SignIn";
+import SignUp from "../Pages/Auth/SignUp";
 import Home from "../Pages/Home/Home";
 
 const router = createBrowserRouter([
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
   },
 ]);
 export default router;
